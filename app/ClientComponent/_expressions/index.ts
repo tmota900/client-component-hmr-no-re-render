0d053c6259783e7530a3966952bff9ada6a1ff33
Tiago Mota <tiago.mota.900@gmail.com>
2025-08-06 19:48:58 +0100

    chore: add ssr example working

--- app/ClientComponent/_expressions/index.ts
+++ app/ClientComponent/_expressions/index.ts
@@ -1,3 +1,3 @@
 export function FunctionTest() {
-  return "FunctionTest12313";
+  return "FunctionTest";
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { ClientBind } from "./ClientComponent";
+import { FunctionTest } from "./ClientComponent/_expressions"
 
-/** Add your relevant code here for the issue to reproduce */
+/** Changing the FunctionTest body/result, 
+ * won't trigger a render on the client component*/
 export default function Home() {
-  return <div><ClientBind /></div>
+  return <div>
+    <ClientBind />
+    <h2>{FunctionTest()}</h2>
+  </div>
 }
